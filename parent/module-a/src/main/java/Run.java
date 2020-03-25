bf5c767521a74b7febd5a88f3b7ee8c10078af46
lkadlec <lkadlec@amdocs.com>
2020-03-25 09:36:56 +0100

    initial commit

---
+++ parent/module-a/src/main/java/Run.java
@@ -0,0 +1,9 @@
+public class Run
+{
+    public static void main(String[] args) {
+        System.out.println("Started");
+        Person john = new Person("John");
+        System.out.println("Who is this: " + john);
+    }
+
+}
